5fee703d8de194f8fcee8f7dd27173cb6ef6a352
Charles Roth <charlieroth4@icloud.com>
2021-02-27 12:07:54 -0500

    Added gen_stage dep, :crypto

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Scraper.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :crypto],
       mod: {Scraper.Application, []}
     ]
   end
@@ -24,6 +24,7 @@ defmodule Scraper.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:gen_stage, "~> 1.1"}
     ]
   end
 end
