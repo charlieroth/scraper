d893f17d10c366f12c58d1d3ad4a40486fe24605
Charles Roth <charlieroth4@icloud.com>
2021-04-03 09:52:36 -0400

    Added broadway

--- mix.exs
+++ mix.exs
@@ -23,7 +23,8 @@ defmodule Scraper.MixProject do
   defp deps do
     [
       {:gen_stage, "~> 1.1"},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:broadway, "~> 0.6"}
     ]
   end
 end
